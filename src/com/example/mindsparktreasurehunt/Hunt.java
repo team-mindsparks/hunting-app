ab5eb180431f71b77d3a86cf6d36f930b2ea5d77
Dwain Faithfull <dwfaithfull@gmail.com>
2014-02-08 07:57:52 +0000

    Initial app with mocked out hunts

---
+++ src/com/example/mindsparktreasurehunt/Hunt.java
@@ -0,0 +1,9 @@
+package com.example.mindsparktreasurehunt;
+
+public class Hunt {
+	public String name;
+	
+	public String toString() {
+		return name;
+	}
+}
---
+++ src/com/example/mindsparktreasurehunt/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.mindsparktreasurehunt;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		populateTreasureHunts();
+
+	}
+	
+	
+	private void populateTreasureHunts() {
+		ListView lv = (ListView) findViewById(R.id.treasureHuntList);
+		Type listType = new TypeToken<List<Hunt>>() {}.getType();
+		List<Hunt> yourList = new Gson().fromJson(treasureHuntJson(), listType);
+		
+		ArrayAdapter<Hunt> adapter = new ArrayAdapter<Hunt>(this,
+		        android.R.layout.simple_list_item_1, yourList);
+		 lv.setAdapter(adapter);
+	}
+	
+	private String treasureHuntJson() {
+		return "[{\"name\" : \"A Museum\" },{\"name\" : \"Playground\" },{\"name\" : \"Forest Trail\" },{\"name\" : \"Playcenter\" }]";
+	}
+
+}
