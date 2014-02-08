7e545022df7c3eeb84b42d5d4c0ad691d68d1805
Daniel Green <dan2552@gmail.com>
2014-02-08 19:07:34 +0000

    Refactor MainActivity

--- src/com/example/mindsparktreasurehunt/MainActivity.java
+++ src/com/example/mindsparktreasurehunt/MainActivity.java
@@ -1,21 +1,10 @@
 package com.example.mindsparktreasurehunt;
 
-import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -23,29 +12,29 @@ import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
+	ListView listView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-//		populateTreasureHunts();
-//
-//		final ListView lv = (ListView) findViewById(R.id.treasureHuntList);
-//
-//	    lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//
-//	        @Override
-//	        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//	          final Hunt item = (Hunt) parent.getItemAtPosition(position);
-//	          Log.e("APP", "" + item.toString());
-//	        }
-//	      });
-
+		prepareViews();
+	}
+	
+	private void prepareViews() {
+		listView = (ListView) findViewById(R.id.treasureHuntList);
+		listView.setOnItemClickListener(listViewItemClickListener);
 		populateTreasureHunts();
 	}
 	
-	
-	
-	
+	AdapterView.OnItemClickListener listViewItemClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+          final Hunt item = (Hunt) parent.getItemAtPosition(position);
+          Log.e("APP", "" + item.toString());
+        }
+	};
+
 	private void populateTreasureHunts() {
 		ArrayList<BaseModel> hunts = BaseModel.deserializeArray(treasureHuntJson(), Hunt.class);
 		
