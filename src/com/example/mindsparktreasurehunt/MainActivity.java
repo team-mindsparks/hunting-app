75057a11a4e131fe0f642ecf378be3c2425a1f37
Dwain Faithfull <dwfaithfull@gmail.com>
2014-02-08 11:13:54 +0000

    Mock out hunts

--- src/com/example/mindsparktreasurehunt/MainActivity.java
+++ src/com/example/mindsparktreasurehunt/MainActivity.java
@@ -10,6 +10,8 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -20,6 +22,17 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		populateTreasureHunts();
+
+		final ListView lv = (ListView) findViewById(R.id.treasureHuntList);
+
+	    lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+	        @Override
+	        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+	          final Hunt item = (Hunt) parent.getItemAtPosition(position);
+	          Log.e("APP", "" + item.toString());
+	        }
+	      });
 
 	}
 	
